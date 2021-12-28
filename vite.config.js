0b14d6a2ab5fe351976bfd611246d92b40eef1fc
kellycastrof <castrofigueroakelly@gmail.com>
2021-12-28 08:56:27 -0500

    feat: KPI-86 add mode config

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,23 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@components': path.resolve(__dirname, 'src/components')
+export default defineConfig(({ mode }) => {
+  return {
+    mode: mode,
+    plugins: [react()],
+    server: {
+      host: true
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@components': path.resolve(__dirname, 'src/components')
+      }
+    },
+    test: {
+      global: true,
+      environment: 'happy-dom'
     }
-  },
-  test: {
-    global: true,
-    environment: 'happy-dom'
   }
 })
