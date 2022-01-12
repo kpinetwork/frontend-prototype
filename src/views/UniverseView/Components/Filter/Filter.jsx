33ac5135b4ba8ed93defb219664760db9d957b36
Daniela Garcia <70675131+dsgarcia8@users.noreply.github.com>
2022-01-12 12:18:44 -0500

    feat: KPI-90 modify filter distribution in medium resolution

--- src/views/UniverseView/Components/Filter/Filter.jsx
+++ src/views/UniverseView/Components/Filter/Filter.jsx
@@ -18,19 +18,19 @@ export function Filter ({ setFilters, fillFilters, filters }) {
   return (
     <CardKPI title={'Filters'} actions={false} overflow={'auto'} textAlign={'start'}>
         <Grid container >
-            <Grid item xs={12} sm={10} md ={10} lg={4} xl={4} style={{marginTop: 20}}>
+            <Grid item xs={12} sm={10} md ={6} lg={4} xl={4} style={{marginTop: 20}}>
                 <Sectors setFilters={setFilters} fillFilters={fillFilters} selectedList={sector}/>
             </Grid>
-            <Grid item xs={12} sm={10} md ={10} lg={4} xl={4} style={{marginTop: 20}}>
+            <Grid item xs={12} sm={10} md ={6} lg={4} xl={4} style={{marginTop: 20}}>
                 <Investor setFilters={setFilters} fillFilters={fillFilters} selectedList={investor}/>
             </Grid>
-            <Grid item xs={12} sm={10} md ={10} lg={4} xl={4} style={{marginTop: 20}}>
+            <Grid item xs={12} sm={10} md ={6} lg={4} xl={4} style={{marginTop: 20}}>
                <Size setFilters={setFilters}fillFilters={fillFilters} selectedList={size}/>
             </Grid>
-            <Grid item xs={12} sm={10} md ={10} lg={6} xl={6} style={{marginTop: 50}}>
+            <Grid item xs={12} sm={10} md ={6} lg={6} xl={6} style={{marginTop: 50}}>
                 <Growth setFilters={setFilters} fillFilters={fillFilters} selectedList={growth}/>
             </Grid>
-            <Grid item xs={12} sm={10} md ={10} lg={12} xl={10} style={{marginTop: 20}}>
+            <Grid item xs={12} sm={10} md ={6} lg={12} xl={10} style={{marginTop: 20}}>
                 <Vertical setFilters={setFilters} fillFilters={fillFilters} selectedList={vertical}/>
             </Grid>
         </Grid>
