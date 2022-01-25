ef959deaa34d794050ee224db9a1150c4ee55a05
Daniela Garcia <70675131+dsgarcia8@users.noreply.github.com>
2022-01-25 14:06:28 -0500

    feat: KPI-91 add Google Login
    * feat: KPI-86 move to KPI domain (#5)
    
    * feat: KPi-86 add infrastructure files
    
    * feat: KPI-86 add mode config
    
    * feat: KPI-86 add pipeline
    
    * feat: KPI-86 add template
    
    * test: KPI-86 change vitest, use jest
    
    * feat: KPI-86 change build in pipeline
    
    * feat: KPI-89 import amplify config (#7)
    
    * feat: KPI-89 import amplify config
    
    * fix: KPI-89 fix problem with build
    
    * feat: KPI-94 change domains (#9)
    
    * feat: KPI-94 change domains
    
    * feat: KPI-94 remove root domain and add sub domain app.
    
    * feat: KPI-90 add filters to every page, disable zoom in bubble chart
    
    * fix: KPI-90 re order size by cohort tables
    
    * feat: KPI-90 left justify label in checkbox
    
    * feat: KPI-90 modify filter distribution in medium resolution
    
    * feat: KPI-90 add year selection componente in each screen
    
    * fix: KPI-90 add validation to peers information in comparison view
    
    * fix: KPI-90 fix problem with key
    
    * feat: KPI-53 add authorization in headers (#12)
    
    * feat: KPI-90 format code
    
    * feat: KPI-91 add google authentication (#15)
    
    * feat: KPI-91 add login mechanism
    
    * feat: KPI-91 add aws updated configuration to get google sign in
    
    * fix: KPI-91 fix formatting errors and replace if block with ternary operators
    
    * feat: KPI-89 import amplify config (#7)
    
    * feat: KPI-89 import amplify config
    
    * fix: KPI-89 fix problem with build
    
    * feat: KPI-90 add filters to every page, disable zoom in bubble chart
    
    * fix: KPI-90 re order size by cohort tables
    
    * feat: KPI-90 left justify label in checkbox
    
    * feat: KPI-90 add year selection componente in each screen
    
    * feat: KPI-90 format code
    
    * fix: KPI-91 solve conflicts
    
    * fix: KPI-91 solve conflicts
    
    * feat: KPI-89 import amplify config (#7)
    
    * feat: KPI-89 import amplify config
    
    * fix: KPI-89 fix problem with build
    
    * feat: KPI-90 add filters to every page, disable zoom in bubble chart
    
    * fix: KPI-90 re order size by cohort tables
    
    * feat: KPI-90 left justify label in checkbox
    
    * feat: KPI-90 add year selection componente in each screen
    
    * feat: KPI-90 format code
    
    * feat: KPI-91 add google authentication (#15)
    
    * feat: KPI-91 add login mechanism
    
    * feat: KPI-91 add aws updated configuration to get google sign in
    
    * fix: KPI-91 fix formatting errors and replace if block with ternary operators
    
    * feat: KPI-89 import amplify config (#7)
    
    * feat: KPI-89 import amplify config
    
    * fix: KPI-89 fix problem with build
    
    * feat: KPI-90 add filters to every page, disable zoom in bubble chart
    
    * fix: KPI-90 re order size by cohort tables
    
    * feat: KPI-90 left justify label in checkbox
    
    * feat: KPI-90 add year selection componente in each screen
    
    * feat: KPI-90 format code
    
    * fix: KPI-91 solve conflicts
    
    * fix: KPI-91 solve conflicts
    
    * feat: KPI-95 changes in bubble label and landing page (#14)
    
    * feat: KPI-95 changes in bubble label and landing page
    
    * feat: KPI-95 change colors
    
    * feat: kPI-95 NA when empty data
    
    Co-authored-by: Kelly Castro <castrofigueroakelly@gmail.com>

--- src/utils/sortSizeCohort.js
+++ src/utils/sortSizeCohort.js
@@ -1,15 +1,15 @@
 export const order = {
-    '<$10 million':0,
-    '$10-<30 million':1,
-    '$30-<$50 million':2,
-    '$50-$100 million':3,
-    '$100 million+':4,
-        }
+  '<$10 million': 0,
+  '$10-<30 million': 1,
+  '$30-<$50 million': 2,
+  '$50-$100 million': 3,
+  '$100 million+': 4
+}
 
 export const sortByKey = (array, key) => {
-    return array.sort((a, b) => {
-        let x = a[key];
-        let y = b[key];
-        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-        });
- }        
+  return array.sort((a, b) => {
+    const x = a[key]
+    const y = b[key]
+    return ((x < y) ? -1 : ((x > y) ? 1 : 0))
+  })
+}
