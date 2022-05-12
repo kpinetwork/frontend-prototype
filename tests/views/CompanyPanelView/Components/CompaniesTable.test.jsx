6f35743fc077487edccbdddb4f2bf1b1b5cbb35e
Daniela Garcia <70675131+dsgarcia8@users.noreply.github.com>
2022-05-12 10:43:07 -0500

    feat: KPI-127 add companiesToChange hooks and service tests

--- tests/views/CompanyPanelView/Components/CompaniesTable.test.jsx
+++ tests/views/CompanyPanelView/Components/CompaniesTable.test.jsx
@@ -28,27 +28,6 @@ const hookResponse = {
 
 jest.mock('../../../../src/hooks/useCompanyPanel')
 
-// jest.mock('../../../../src/service/companyPanel', () => {
-//   return {
-//     getCompanyPanel: {
-//       total: 1,
-//       companies: [
-//         {
-//           id: '1234',
-//           name: 'Sample company',
-//           sector: 'test sector',
-//           vertical: 'vertical sector',
-//           inves_profile_name: 'test invest profile',
-//           is_public: false
-//         }
-//       ],
-//       setCompanies: jest.fn(),
-//       isLoading: false,
-//       getCompanyPanel: jest.fn()
-//     }
-//   }
-// })
-
 jest.spyOn(Auth, 'currentAuthenticatedUser').mockReturnValue({
   getAccessToken: () => ({
     getJwtToken: () => ('Secret-Token')
@@ -110,33 +89,6 @@ describe('<CompaniesPanelTable />', () => {
         expect(checkbox.checked).toBeFalsy()
         expect(cancelButton).not.toBeInTheDocument()
       })
-      // it('Click on next page', async () => {
-      //   const response = {
-      //     ...hookResponse,
-      //     getCompanyPanel: hookResponse,
-      //     setCompanies: [{
-      //       id: 10,
-      //       name: 'Sample company',
-      //       sector: 'test sector',
-      //       vertical: 'vertical sector',
-      //       inves_profile_name: 'test invest profile',
-      //       is_public: false
-      //     }],
-      //     companies: Array(11).fill(hookResponse.companies[0]).map(
-      //       (company, index) => (
-      //         { ...company, id: index }
-      //       )
-      //     )
-      //   }
-      //   useCompanyPanel.mockImplementation(() => response)
-      //   setUp()
-      //   const nextPage = screen.getByTitle('Next page')
-      //   screen.debug()
-      //   // fireEvent.click(nextPage)
-      //   await waitFor(() => {
-      //     fireEvent.click(nextPage)
-      //   })
-      // })
     })
   })
 })
