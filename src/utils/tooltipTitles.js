ff09aa84242bbd83892c3491703d500482c0f419
Kelly Castro <castrofigueroakelly@gmail.com>
2022-05-12 12:11:11 -0500

    feat: KPI-163 tooltips (#78)

---
+++ src/utils/tooltipTitles.js
@@ -0,0 +1,11 @@
+export const tooltipTitles = {
+  revenue: 'Annual revenue, selected year',
+  ebitda: 'Annual ebitda, selected year',
+  growth: 'Year-over-year revenue growth, selected year',
+  ebitda_margin: 'Ebitda as % of revenue, selected year',
+  revenue_vs_budget: 'Actual revenue versus budget, selected year',
+  ebitda_vs_budget: 'Actual ebitda versus budget, selected year',
+  rule_of_40: 'Revenue growth plus ebitda margin, selected year',
+  size_cohort: 'Annual revenue, selected year',
+  growth_cohort: 'Year-over-year revenue growth, selected year'
+}
