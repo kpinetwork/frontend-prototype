d9c62c9bb0648baf6862885c539cd467d4c6df55
kelly <castrofigueroakelly@gmail.com>
2022-07-10 17:25:49 -0500

    fix: KPI-194 fix invalid format modal failure when fromEdit is false

--- tests/views/UploadFileView/Components/InvalidFormatModal.test.jsx
+++ tests/views/UploadFileView/Components/InvalidFormatModal.test.jsx
@@ -4,20 +4,18 @@ import '@testing-library/jest-dom'
 
 import InvalidFormatModal from '../../../../src/views/UploadFileView/Components/InvalidFormatModal'
 
-const props = {
+const defaultProps = {
   open: true,
   onClose: () => {},
   errorObject: {
     0: [1, 4, 6],
     1: [],
-    2: [3, 6, 7],
-    4: [],
-    5: []
+    4: []
   }
 }
 
-const setUp = () => {
-  render(<InvalidFormatModal {...props}/>)
+const setUp = (props) => {
+  render(<InvalidFormatModal {...defaultProps} {...props}/>)
 }
 
 describe('<InvalidPreviewModal />', () => {
@@ -31,9 +29,44 @@ describe('<InvalidPreviewModal />', () => {
   it('Should render preview modal with repeated scenarios data', () => {
     setUp()
     const dialog = screen.getByRole('dialog')
-    const scenario = screen.getByText('4, 6')
+    const scenario = screen.getByText('4')
 
     expect(dialog).toBeInTheDocument()
     expect(scenario).toBeInTheDocument()
+  })
+
+  it('Should render preview modal with company names details when comes from edit modify', () => {
+    const companyName = 'Sample company'
+    const props = {
+      body: [
+        { name: companyName },
+        {},
+        {}
+      ],
+      fromModify: true
+    }
+    setUp(props)
+    const dialog = screen.getByRole('dialog')
+    const company = screen.getByText(`4 ${companyName}`)
+
+    expect(dialog).toBeInTheDocument()
+    expect(company).toBeInTheDocument()
+  })
+
+  it('Should render preview modal with company index when comes from edit modify', () => {
+    const props = {
+      body: [
+        {},
+        {},
+        {}
+      ],
+      fromModify: true
+    }
+    setUp(props)
+    const dialog = screen.getByRole('dialog')
+    const company = screen.getByText('4')
+
+    expect(dialog).toBeInTheDocument()
+    expect(company).toBeInTheDocument()
   })
 })
