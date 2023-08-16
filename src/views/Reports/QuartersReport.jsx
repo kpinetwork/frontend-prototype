9775e8c781659ab91ea81dbbc13452c7c77f68bd
Daniela Garcia <70675131+dsgarcia8@users.noreply.github.com>
2023-08-16 10:46:53 -0500

    fix: KPI-00 fix decimals format in quarters report

--- src/views/Reports/QuartersReport.jsx
+++ src/views/Reports/QuartersReport.jsx
@@ -244,7 +244,9 @@ export const QuartersReport = ({ fromUniverseOverview }) => {
     if (value === 'NA' || isNaN(value)) {
       return value
     }
-    if (actualMetric.name === 'CAC ratio' || actualMetric.name === 'CLV/CAC ratio') value = value.toFixed(1) + 'x'
+
+    value = Number(value).toFixed(2)
+    if (actualMetric.name === 'CAC ratio' || actualMetric.name === 'CLV/CAC ratio') value = value.toFixed(2) + 'x'
 
     if (property === 'vs') return value + '%'
     return (actualMetric?.position === 'left' ? actualMetric?.symbol + value : value + actualMetric?.symbol)
